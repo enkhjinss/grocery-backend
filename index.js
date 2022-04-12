3d4fccabc37d9d9aa29c49c6e17e67e326938c01
enkhjinss <enkhjinnasanbayar7@gmail.com>
2022-04-12 14:10:50 +0800

    delete, edit and add product

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
+import { async } from "@firebase/util";
 import express from "express";
-import { collection, getDocs, addDoc } from "firebase/firestore";
+import {
+    collection,
+    getDocs,
+    addDoc,
+    getDoc,
+    doc,
+    setDoc,
+    deleteDoc
+} from "firebase/firestore";
 import { db } from "./firebase.js";
 
 const app = express();
-// app.use(express.json())
 const port = 8080;
+
+app.use(express.json());
+// app.use((req, res, next) => {
+//     next();
+// });
 
 app.get("/", (req, res) => {
     res.send("OK");
 });
-
 
 //List product
 app.get("/products", async (req, res) => {
@@ -24,31 +36,63 @@ app.get("/products", async (req, res) => {
     res.send(products);
 });
 
-
-
 //Create product
 app.post("/products", async (req, res) => {
+    const data = req.body;
+    const docRef = await addDoc(collection(db, "products"), data);
 
-    res.send("created");
-});
-//Product detail
-app.get("/products/1", (req, res) => {
-    // get product from database by id
-    res.send({
-        name: "Banana",
-        price: 123.312,
-        description: "Blablabal",
+    res.status(201).send({
+        id: docRef.id,
+        ...data,
     });
 });
+
+const errorHandleWrapper = (fun) => {
+    return async (req, res, next) => {
+        try {
+            return await fun(req, res, next);
+        } catch (e) {
+            next(e);
+        }
+    };
+};
+
+//Product detail
+app.get(
+    "/products/:id",
+    errorHandleWrapper(async (req, res) => {
+        const docRef = doc(db, "products", req.params.id);
+        const docSnap = await getDoc(docRef);
+        if (!docSnap.exists()) {
+            throw new Error(`Document with id not exist}`);
+        }
+
+        res.status(200).send({
+            id: docSnap.id,
+            ...docSnap.data(),
+        });
+    })
+);
+
 //Edit product
-app.patch("/products/:id", (req, res) => {
-    // get product from database by id
-    res.send("edited");
+
+app.patch("/products/:id", async (req, res) => {
+    const docRef = doc(db, "products", req.params.id);
+    await setDoc(docRef, req.body, { merge: true });
+    const docSnapshot = await getDoc(docRef);
+
+    res.send({
+        id: docSnapshot.id,
+        ...docSnapshot.data(),
+    });
 });
+
 //Delete product
-app.delete("/products/:id", (req, res) => {
-    res.send("deleted");
+app.delete("/products/:id", async (req, res) => {
+    await deleteDoc(doc(db, "products", req.params.id));
+    res.send(`${req.params.id} id-tai product ustlaa`);
 });
+
 
 //List categories
 app.get("/categories", (req, res) => {
@@ -69,5 +113,5 @@ app.use("*", (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log("hsj");
+    console.log("started");
 });

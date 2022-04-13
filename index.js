d6301fc67905edd4902231733d9a7984234d3e7a
enkhjinss <enkhjinnasanbayar7@gmail.com>
2022-04-13 11:48:25 +0800

    cors suulgasan

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import { async } from "@firebase/util";
+// import { async } from "@firebase/util";
 import express from "express";
+import cors  from "cors";
 import {
     collection,
     getDocs,
@@ -13,6 +14,7 @@ import { db } from "./firebase.js";
 
 const app = express();
 const port = 8080;
+app.use(cors())
 
 app.use(express.json());
 // app.use((req, res, next) => {

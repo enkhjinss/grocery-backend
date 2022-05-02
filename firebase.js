0a07a6d0017ea32683f0c9eeb2c6da17f64f8396
enkhjinss <enkhjinnasanbayar7@gmail.com>
2022-05-02 13:55:32 +0800

    env file uusgeed congig-oo nuusan, firebase-n 2 uur config holboson

--- firebase.js
+++ firebase.js
@@ -1,14 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
+import dotenv from "dotenv";
+dotenv.config();
 const firebaseConfig = {
-    apiKey: "AIzaSyBhIyIXa7XnTCUlU8xXvkYjzOvatDIwEGY",
+    apiKey: process.env.FIREBASE_API_KEY,
     authDomain: "grocery-store-497ba.firebaseapp.com",
-    projectId: "grocery-store-497ba",
+    projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: "grocery-store-497ba.appspot.com",
     messagingSenderId: "469218615618",
     appId: "1:469218615618:web:4482a88637077a003e28db",
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
